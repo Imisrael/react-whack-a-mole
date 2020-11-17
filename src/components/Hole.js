1e4ac6c42984de721ee8b8a9f26ec49b1f9b3eed
israel <imru@fixstars.com>
2020-11-17 12:26:57 -0800

    added better image aain

--- src/components/Hole.js
+++ src/components/Hole.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import mole from '../images/trump.png';
+import mole from '../images/trump-face.png';
 import classnames from 'classnames';
 import './Hole.css';
 
